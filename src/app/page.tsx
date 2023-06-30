bc6c6423e44ec32f75b02ee228f81c5cf37529e1
Nelson Izquierdo <izquierdonelson@gmail.com>
2023-06-30 01:39:53 -0400

    🛠  refactor: new earnings and expenses

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,33 @@
 import Donut from "@/components/Donut";
 import TableComponent from "@/components/TableComponent";
 import { EarningItem } from "@/utils/types";
-
+const earnings: EarningItem[] = [
+  {
+    type: "earning",
+    value: 30577,
+  },
+  {
+    type: "earning",
+    value: 38676,
+  },
+  {
+    type: "earning",
+    value: 127552,
+  },
+  {
+    type: "earning",
+    value: 100930,
+  },
+  {
+    type: "expense",
+    value: 115000,
+  },
+];
 export default function Home() {
-  const earnings: EarningItem[] = [
-    {
-      type: "earning",
-      value: 30577,
-    },
-    {
-      type: "earning",
-      value: 38676,
-    },
-    {
-      type: "earning",
-      value: 127552,
-    },
-    {
-      type: "earning",
-      value: 37685,
-    },
-    {
-      type: "expense",
-      value: 85000,
-    },
-  ];
-
   return (
-    <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Donut earnings={earnings} />
-        <TableComponent earnings={earnings} />
-      </main>
-    </>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Donut earnings={earnings} />
+      <TableComponent earnings={earnings} />
+    </main>
   );
 }
