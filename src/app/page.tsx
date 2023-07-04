a3b5270c22d5023ed095365e93464ba469112435
Nelson Izquierdo <izquierdonelson@gmail.com>
2023-07-04 17:41:43 -0400

    🆕 feat: sort desc by date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-8 gap-8">
       <Donut earnings={earnings} />
       <TableComponent
-        earnings={[...earnings].sort((a, b) => a.date.localeCompare(b.date))}
+        earnings={[...earnings].sort((a, b) => b.date.localeCompare(a.date))}
       />
     </main>
   );
