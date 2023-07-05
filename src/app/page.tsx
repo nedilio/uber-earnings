bfa165d7536843529a949b02a87606cc7d8f1e2d
Nelson Izquierdo <izquierdonelson@gmail.com>
2023-07-05 14:02:48 -0400

    test fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,16 @@ const baseURL = process.env.API_BASE_URL;
 
 export default async function Home() {
   console.log("fetch data from: ", `${baseURL}/api/earning`);
-  const data = await fetch(`${baseURL}/api/earning`, {
-    cache: "no-store",
-  });
-  const earnings = await data.json();
+  const earnings = [];
+
+  try {
+    const data = await fetch(`${baseURL}/api/earning`, {
+      cache: "no-store",
+    });
+    earnings.push(...(await data.json()));
+  } catch {
+    const earnings = [];
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8 gap-8">
       <Donut earnings={earnings} />
