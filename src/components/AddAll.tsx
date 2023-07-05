00251ffd32ffa5e7d6f99a2ce78d3e1d6f9507d3
Nelson Izquierdo <izquierdonelson@gmail.com>
2023-07-04 22:02:41 -0400

    add al mock data to supabase

---
+++ src/components/AddAll.tsx
@@ -0,0 +1,23 @@
+﻿"use client";
+
+import { earnings } from "@/utils/mockdata";
+
+const AddAll = () => {
+  const handleAddAll = () => {
+    console.log("AddAll");
+    earnings.forEach(async (earning) => {
+      const { id, ...rest } = earning;
+      console.log(rest);
+      fetch("/api/earning", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(rest),
+      });
+    });
+  };
+  return <button onClick={handleAddAll}>AddAll</button>;
+};
+
+export default AddAll;
