a63e11d8338bb9a7c390e46c9d8a36c5b2ec5aed
nelson <n.izquierdo@logicea.com>
2023-08-14 12:39:07 -0400

    🐛 fix: type lint error on deploy

--- src/app/api/feriados/route.ts
+++ src/app/api/feriados/route.ts
@@ -1,10 +1,15 @@
 ﻿import { NextResponse } from "next/server";
 
+interface Feriado {
+  nombre: string;
+  fecha: string;
+}
+
 export async function GET() {
   const res = await fetch("https://apis.digital.gob.cl/fl/feriados/2023", {
     cache: "no-store",
   });
-  const feriados = await res.json();
+  const feriados = (await res.json()) as Feriado[];
   const mapedFeriados = feriados.map((feriado) => ({
     ...feriado,
     fecha: feriado.fecha + " 00:00",
