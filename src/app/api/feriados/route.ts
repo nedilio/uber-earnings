c801d9237ddda660f074302ee110a1c5a33e7137
nelson <n.izquierdo@logicea.com>
2023-08-14 12:26:29 -0400

    🆕 feat: feriados api

---
+++ src/app/api/feriados/route.ts
@@ -0,0 +1,13 @@
+﻿import { NextResponse } from "next/server";
+
+export async function GET() {
+  const res = await fetch("https://apis.digital.gob.cl/fl/feriados/2023", {
+    cache: "no-store",
+  });
+  const feriados = await res.json();
+  const mapedFeriados = feriados.map((feriado) => ({
+    ...feriado,
+    fecha: feriado.fecha + " 00:00",
+  }));
+  return NextResponse.json(mapedFeriados);
+}
